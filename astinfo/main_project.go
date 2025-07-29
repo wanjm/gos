f6f06588564158e757f55bf973263624728a2797
wanjm <wan_jm@126.com>
2025-07-29 21:29:29 +0800

    添加了basic。ERRor

--- astinfo/main_project.go
+++ astinfo/main_project.go
@@ -66,6 +66,16 @@ type Error struct {
 func (error *Error) Error() string {
 	return error.Message
 }
+func New(code int, msg string) error {
+	res := &Error{
+		Code:    code,
+		Message: msg,
+	}
+	return res
+}
+func (error *Error) GetErrorCode() int {
+	return error.Code
+}
 	`), 0660)
 }
 
