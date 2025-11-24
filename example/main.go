8e0615043e1e83fd54953aa605baed016c84f121
wanjm <wan_jm@126.com>
2025-11-24 20:46:00 +0800

    update example code

--- example/main.go
+++ example/main.go
@@ -1,14 +1,34 @@
 package main
-
-import (
-	"github.com/wan_jm/servlet_example/gen"
-)
+import ("flag"
+"github.com/wan_jm/servlet_example/gen")
 
 func main() {
-	wg := gen.Run(gen.Config{
-		Cors:       true,
-		Addr:       ":8080",
-		ServerName: "servlet",
+	parseArgument();
+	run()
+}
+func parseArgument() {
+	flag.Parse()
+}
+func run() {
+	wg:=gen.Run(gen.Config{
+		Cors: true,
+		Addr: ":8080",
+		ServerName: "servlet", // this is the name of group tag in comments;
 	})
 	wg.Wait()
+	/*
+	下面的方法以来 github.com/wanjm/common 包，需要手动添加依赖；使用了优雅退出机制；
+	common.InitLogger()
+	manager := common.GracefulManager
+	shutdown := gen.Start(gen.Config{
+		Cors:       true,
+		Addr:       ":8080",
+		ServerName: "servlet", // this is the name of group tag in comments;
+	})
+	manager.Go("http server shutdown monitor", func(ctx context.Context) {
+		shutdown(ctx, 5*time.Second)
+	})
+	manager.Wait()
+ 	*/
 }
+	
