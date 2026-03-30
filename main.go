825098e98b96efc87bc1c8f69e8e84c8c8bc4118
wan_jm <wan_jm@126.com>
2026-03-30 22:10:59 +0800

    bump version to 0.5.4

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/wanjm/gos/db"
 )
 
-const Version = "0.5.3"
+const Version = "0.5.4"
 
 func parseArgument() {
 	flag.StringVar(&basic.Argument.SourcePath, "p", ".", "需要生成代码工程的根目录")
