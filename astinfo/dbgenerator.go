fc26d7b34bfca877599e6063f6b8d3b3395e85df
wan_jm <wan_jm@126.com>
2026-05-21 22:00:35 +0800

    use module

--- astinfo/dbgenerator.go
+++ astinfo/dbgenerator.go
@@ -273,14 +273,14 @@ type {{.TableName}}Dal struct {
 }
 
 func (a *{{.TableName}}Dal) getDB(ctx context.Context) *gorm.DB {
-	return a.{{.DBVariable}}.WithContext(ctx).Table("{{.RawTableName}}")
+	return a.{{.DBVariable}}.WithContext(ctx).Model(&{{.Pkg.Name}}.{{.TableName}}{})
 }
 	
 func (c *{{.TableName}}Dal) getDBOperation(context context.Context) common.DbOperation {
 	return common.DbOperation{
-		Db:        c.{{.DBVariable}},
-		TableName: "{{.RawTableName}}",
-		Context:   context,
+		Db:      c.{{.DBVariable}},
+		Module:  &{{.Pkg.Name}}.{{.TableName}}{},
+		Context: context,
 	}
 }
 
